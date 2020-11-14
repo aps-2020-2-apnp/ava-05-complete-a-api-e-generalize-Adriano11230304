62e3a18f01f6c5a6242db20147e61eb9cfd46a23
adriano11230304 <adriano.al.pereira1@gmail.com>
2020-11-14 19:22:59 -0300

    Pronto

--- webservice/src/main.ts
+++ webservice/src/main.ts
@@ -5,8 +5,17 @@ interface Usuario {
   nome: string
   sobrenome: string
 }
+
+interface Produto {
+  descricao: string
+  preco: number
+}
 let usuarioCont = 0
 const usuarios: Record<number, Usuario> = {}
+let produtoCont = 0
+const produtos: Record<number, Produto> = {}
+
+const porta = 9999;
 
 const servidor = createServer((req, resp) => {
   const url = parse(req.url ?? '', true)
@@ -53,11 +62,51 @@ const servidor = createServer((req, resp) => {
           }
         }
       }
+      resp.writeHead(404)
+      resp.end('Not Found (Nao Encontrado)')
     }
     case '/produtos': {
+      if (req.method === 'GET') {
+        resp.writeHead(200, {'Content-Type': 'application/json'})
+        resp.end(JSON.stringify(produtos))
+        break
+      } else if (req.method === 'POST') {
+        let corpo = ''
+        req.on('data', (parte) => corpo += parte)
+        req.on('end', () => {
+          const obj = JSON.parse(corpo)
+          produtos[++produtoCont] = obj
+          resp.writeHead(200, { 'Content-Type': 'text/plain' })
+          resp.end('Produto Salvo')
+        })
+        break
+      }
     }
     case '/produto': {
-
+      if (req.method === 'GET') {
+        if (url.query.id) {
+          if (typeof (url.query.id) === 'string') {
+            const id = parseInt(url.query.id, 10)
+            if (produtos[id]) {
+              resp.writeHead(200, { 'Content-Type': 'application/json' })
+              resp.end(JSON.stringify(produtos[id]))
+              break
+            }
+          }
+        }
+      } else if (req.method === 'DELETE') {
+        if (url.query.id) {
+          if (typeof (url.query.id) === 'string') {
+            const id = parseInt(url.query.id, 10)
+            if (produtos[id]) {
+              resp.writeHead(200, { 'Content-Type': 'text/plain' })
+              delete produtos[id]
+              resp.end('Produto Deletado')
+              break
+            }
+          }
+        }
+      }
     }
     default: {
       resp.writeHead(404)
@@ -66,6 +115,6 @@ const servidor = createServer((req, resp) => {
   }
 })
 
-servidor.listen(9999, () => {
+servidor.listen(porta, () => {
   console.log('Servidor rodando em http://localhost:9999')
 })
